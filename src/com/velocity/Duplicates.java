6b19cf4d1a44de818e514b05748540100cf5cbd2
monikaawari <monikaawari14@gmail.com>
2023-01-19 11:06:19 +0530

    new class added

---
+++ src/com/velocity/Duplicates.java
@@ -0,0 +1,19 @@
+package com.velocity;
+
+public class Duplicates {
+	public static void main(String[] args) {
+		String s="hello everyone";
+		char[]ch=s.toCharArray();
+		for(int i=0;i<ch.length-1;i++) {
+			for(int j=i+1;j<ch.length-1;j++) {
+				if(ch[i]==ch[j]&& ch[i]!=' ') {
+					ch[j]='0';
+				}
+			}
+			if(ch[i]!='0') {
+			System.out.print(ch[i]);
+		}}
+		
+	}
+
+}
--- src/com/velocity/Test.java
+++ src/com/velocity/Test.java
@@ -1,7 +1,21 @@
 package com.velocity;
 
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class Test {
 public static void main(String[] args) {
-	System.out.println("I am Software Devloper");
+
+	String s="avali lovely kohali";
+	char[]ch=s.toCharArray();
+Set<Character>set=new HashSet<Character>();
+Set<Character>duplicates=new LinkedHashSet<Character>();
+for(char c:ch) {
+	if(!set.add(c)&& c!=' ') {
+		duplicates.add(c);
+	}
+}
+System.out.println(duplicates);
 }
 }
